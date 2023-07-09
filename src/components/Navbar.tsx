4974c56e5d7480263ca726278e2b283dc0969344
Markjust1 <novikov711@gmail.com>
2023-07-09 14:32:54 -0600

    add selection of jobsites + styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,15 @@ const Navbar: React.FC = () => {
   return (
     <div className={styles.container}>
       <img src={icon}></img>
+      <div className={styles.column}>
+        <div className={styles.user}>Select Jobsite</div>
+        <select>
+          <option>Site 1</option>
+          <option>Site 2</option>
+          <option>Site 3</option>
+          <option>Site 4</option>
+        </select>
+      </div>
       <div className={styles.user}>Artem Novikov</div>
     </div>
   );
